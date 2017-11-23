8c13766a121c75b986e9cf51e6314ba12087c846
tomas <tomasbortoli@gmail.com>
2017-11-23 17:56:15 +0100

    Fix issue https://github.com/samwize/python-email-crawler/issues/17
    Signed-off-by: tomas <tomasbortoli@gmail.com>

--- email_crawler.py
+++ email_crawler.py
@@ -50,7 +50,7 @@ def crawl(keywords):
 			Store the HTML
 	"""
 	logger.info("-"*40)
-	logger.info("Keywords to Google for: %s" % keywords)
+	logger.info("Keywords to Google for: %s" % keywords.decode('utf-8'))
 	logger.info("-"*40)
 
 	# Step 1: Crawl Google Page
